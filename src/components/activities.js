6e7a18cee4f451be6385926b69be886f0d9e61cc
Kieran McGuinness <zumaht@gmail.com>
2022-03-17 19:24:25 -0400

    index edits

---
+++ src/components/activities.js
@@ -0,0 +1,15 @@
+
+
+const CreateActivities = () => {
+
+    return (
+        <div className="CreateAc">
+            <p>Hello World</p>
+            <p>How are you today</p>
+
+        </div>
+    );
+}
+
+
+export default CreateActivities
