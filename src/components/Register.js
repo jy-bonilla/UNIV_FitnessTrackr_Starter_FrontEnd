79c25e0b6ad3422fa6b619fe0996515f2eed9859
jamesaudry <james.audry@gmail.com>
2022-03-19 18:42:04 -0400

    not working yet

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,54 +1,66 @@
-// import React, { useState } from "react";
-// import { Route } from "react-router";
-// import { callAPI } from '../api';
+import React, { useState } from "react";
 
-// const Register = ({ setToken }) => {
-//   const [ newUsername, setNewUsername ] = useState('');
-//   const [ newPassword, setNewPassword ] = useState('');
-//   const history = Route();
 
-//   const handleRegisterSubmit = async (ev) => {
-//     ev.preventDefault();
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCon, setPasswordCon] = useState('');
 
-//     try {
-//       const registerObj = await callAPI({
-//         method: 'POST',
-//         url: 'users/register',
-//         body: {
-//           username: `${newUsername}`,
-//           password: `${newPassword}`
-//         }
-//       });
+    const handleSubmit = (event) => {
 
-//       setNewUsername('');
-//       setNewPassword('');
+        //prevents page from being submitted
+        event.preventDefault()
+        // console.log('username: ', username);
+        // console.log('password: ', password);
+        // console.log('passwordCon:', passwordCon)
 
-//       if (registerObj.token) {
-//         setToken(registerObj.token);
-//         history.push('/');
-//       }
+        //checks to make sure password and passwordConfirmation match => fetch call else deny fetch call
+        if (password !== passwordCon) {
+            alert("Passwords don't match!");
+        } else {
 
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
 
-//   return <div>
-//     <h2 className='header'>Register</h2>
-//     <form onSubmit={handleRegisterSubmit}>
-//       <label htmlFor='new-username'>Username:</label>
-//       <input placeholder='New Username' type='text' value={newUsername} onChange={
-//         ev => setNewUsername(ev.target.value)
-//       }></input>
+            //Register a user by submitting form info to POST /api/COHORT-NAME/users/register
 
-//       <label htmlFor='new-password'>Password:</label>
-//       <input placeholder='New Password' type='password' /*minLength='8'*/ value={newPassword} onChange={
-//         ev => setNewPassword(ev.target.value)
-//       }></input>
+            fetch('http://fitnesstrac-kr.herokuapp.com/api/users/register', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            }).then(response => response.json())
+                .then(result => {
+                    console.log(result);
+                    // saves the token from the api to a const and saves it on localStorage
+                    const { token } = result.data;
+                    localStorage.setItem('token', token);
 
-//       <button type='submit'>Register</button>
-//     </form>
-//   </div>
-// }
+                })
+                .catch(console.error);
+        }
+    }
 
-// export default Register;
+    return (
+        <div className="navbar-container" id='container'>
+            <div className="navbar-register" id='navbar'>
+                <h2>Register New Account</h2>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='username'>Username:</label>
+                <input type='text' required name='username' value={username} onChange={(event) => setUsername(event.target.value)} />
+                <label htmlFor='password'>Password:</label>
+                <input type='password' required minLength='4' name='password' value={password} onChange={(event) => setPassword(event.target.value)} />
+                <label htmlFor='password-confirm'>Confirm Password:</label>
+                <input type='password' required minLength='4' name='password-confirm' value={passwordCon} onChange={(event) => setPasswordCon(event.target.value)} />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+
+
+export default Register;
